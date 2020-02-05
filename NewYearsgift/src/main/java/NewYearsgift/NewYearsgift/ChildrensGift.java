ae13e980e941ffe6eb3d3980913e89932c8cb4fb
neellohit <neellohit69@gmail.com>
2020-02-05 22:41:17 +0530

    NewYearsGift v1.0

--- NewYearsgift/src/main/java/NewYearsgift/NewYearsgift/ChildrensGift.java
+++ NewYearsgift/src/main/java/NewYearsgift/NewYearsgift/ChildrensGift.java
@@ -2,52 +2,50 @@ package NewYearsgift.NewYearsgift;
 import java.util.*;
 public class ChildrensGift {
     // ChildrensGift Collection of Objects
-	 static ArrayList<Object> l=new ArrayList<>();
-	 ChildrensGift(int n,String type,int w)
+	 static ArrayList<sweet> l=new ArrayList<>();
+	 ChildrensGift(String type,int w)
 	 {
 		
-		    if(n==1) // chocolates Gift
-		    {
-		    if(w>50) {  // weight > 50 falls under CocoaChocolate
+		   
+		    if(w>=75 && w<=150) {  // weight > 75 falls under CocoaChocolate
 	 		Random r=new Random();
 		   	int k=r.nextInt(500);
-		   	CocoaChocolate c1=new CocoaChocolate(w,k);
-		   	c1.setForm("choco-Bar"); // all CocoaChocolate are choco-bars
+		   	sweet c1=new chocolate(w,k);
 		   	c1.setType(type);
+		   	c1.present();
 		    l.add(c1);
 		    }
-		    else
+
+		    
+		    else if(w<75)
 		    {
-		    Random r=new Random();   // weight < 50 falls under Candy
+		    Random r=new Random();   // weight < 75 falls under Candy
 		   	int k=r.nextInt(50);
-		   	Candies c1=new Candies(w,k);
-		   	c1.setForm("candy-wrap"); // all Candies are candy-wrap
+		   	sweet c1=new Candies(w,k);
 		   	c1.setType(type);
+		   	c1.present();
 		    l.add(c1);
-		    }
 		    }
 		    
-		   else if(n==2)    // Sweets gift
-		    {
-			 
-		    if(w>50)  // weight > 50 falls under Laddu
+		      // Sweets gift
+		    else if(w>200)  // weight > 200 falls under Laddu
 		    {
 		 	 Random r=new Random();
 		   	int k=r.nextInt(500);
-		   	Laddu c1=new Laddu(w,k);
+		   	sweet c1=new Laddu(w,k);
 		   	c1.setType(type);
-		    l.add(c1);   // all Laddus are plain boxes
-		 	 }
-		    else
-		    {
-		    Random r=new Random();   // weight < 50 falls under MilkSweets
- 		   	int k=r.nextInt(50);
- 		 	Milksweet c1=new Milksweet(w,k);
- 		 	c1.present("gift-wrap");  // all MilkSweets are gift-wraps
- 		 	c1.setType(type, 2020);
-		    l.add(c1);
+		   	c1.present();
+		    l.add(c1);   
 		 	}
-		    }
+		    
+		    else if(w>150 && w<=200)
+		    {
+		    Random r=new Random();   // weight >150 falls under GulabJamun
+ 		   	int k=r.nextInt(50);
+ 		 	sweet c1=new GulabJamun(w,k);
+		    l.add(c1);
+		    c1.present();
+		    } 
 		    else    // Wrong input 
 		    {
 		    System.out.println("sorry this gift is not for you!");	
@@ -62,45 +60,42 @@ public class ChildrensGift {
     	l+=1;
     	System.out.println("You will recieve "+l+" gifts");
     	System.out.println("Which type of Gift do you want?");
-    	System.out.println("Enter 1 for chocolate and enter the type of chocolate and its weight");
-    	System.out.println("Enter 2 for sweet and enter the type of sweet and its weight");
+    	System.out.println("Enter the sweet and its weight");
+    	System.out.println("All Chocolates and candies should have weights less than 150");
     	
     	Scanner sc=new Scanner(System.in);
     	for(int i=0;i<l;i++)
     	{
     		String s=sc.nextLine();
        // Create a new ChildrensGift object
-    		new ChildrensGift(Integer.parseInt(s.split(" ")[0]),s.split(" ")[1],Integer.parseInt(s.split(" ")[2]));
+    		new ChildrensGift(s.split(" ")[0],Integer.parseInt(s.split(" ")[1]));
     	}
     	sc.close();
     	// Output  weights,calories and form of gifts
-    	 for(Object str: ChildrensGift.l) {
+    	 for(sweet str: ChildrensGift.l) {
     		 System.out.println(str);
     		 System.out.println("---------------------------+");
     	 }
-    	 
     	int w=0;
-    	 for(Object str: ChildrensGift.l){
-    		 if (str instanceof chocolate)
- 			w+=((chocolate)str).weight;
-    		 else
-    		w+=((sweet)str).weight;	
+    	 for(sweet str: ChildrensGift.l){
+    		w+=str.weight;	
  	        }
     	 System.out.println("Total weight of gifts is: "+w);
-    	 System.out.println("--------------------------------");
+    	 System.out.println("-----------------------------");
     	 
-    	int c=0,ch=0;
-    	 	 for(Object str: ChildrensGift.l){
-    		 if (str instanceof chocolate) {
- 			if(((chocolate)str).weight>50) {
- 				System.out.println(((chocolate)str).getType()+" is a chocolate");
+    	// identify the chocolates and candies
+    	 int c=0,ch=0;
+    	 	 for(sweet str: ChildrensGift.l){
+    		 
+ 			if(str.weight>=75 && str.weight<=150) {
+ 				System.out.println(str.getType()+" is a chocolate");
  			ch=1;
  			}
-    		 else {
-    			 System.out.println(((chocolate)str).getType()+" is a Candy");
+    		 if(str.weight<75) {
+    			 System.out.println(str.getType()+" is a Candy");
     			 c=1;
     		 }
- 			}
+ 			
  	   }
     	 	 if(c==0)
     	 	System.out.println("No Candy Gift!");
